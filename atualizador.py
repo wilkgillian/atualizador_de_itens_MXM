e4feed014c7f7e5f6c64230731fa26b40d56b9bf
wilk.gillian <wilk.silva@mt.senac.br>
2022-09-01 12:42:43 -0300

    finished

--- atualizador.py
+++ atualizador.py
@@ -1,67 +1,74 @@
-from email.utils import format_datetime
 import pyautogui
 import time
-from datetime import datetime
-from playwright.sync_api import sync_playwright
+import asyncio
+from playwright.async_api import async_playwright
 import pandas as pd
-from bs4 import BeautifulSoup
-import re
-import openpyxl
 
 t1 = time.time()
-planilha = pd.read_excel("base_de_dados/Produtos para atualizar.xlsx", "Produtos", usecols=['Código', 'Produtos'], dtype='string')
-with sync_playwright() as p:
-      browser = p.chromium.launch(headless=False, timeout=5000)
-      context = browser.new_context()
-      page = context.new_page()
-      page.goto("https://sistemas.mt.senac.br/")
-      with context.expect_event("page") as event_info:
-          page.locator("//img[@src='assets_sistemas\img\sistemas\mxm-treinamento.png']").click()
-      mxm = event_info.value
-      mxm.locator("#txfUsuario").fill("WLIK.SILVA")
-      mxm.locator("#txfSenha").fill("Alterar@2022")
-      mxm.locator("#ext-gen19").click()
-      avaliacao = pyautogui.locateOnScreen('Avaliacao.png', confidence = 0.9)
-      if (avaliacao != None):
-        mxm.locator("//*[@id='conpass-tag']/div/div/div[2]/div[1]/div[1]").click()
-      time.sleep(1)
-      mxm.locator("#tgfBusca").fill("1022")
-      mxm.locator("#ext-gen37").click()
-      time.sleep(2)
-      produto = pyautogui.locateOnScreen('produto.png', confidence = 0.8)
-      botao = pyautogui.moveTo(produto)
-      time.sleep(1)
-      pyautogui.click()
-      time.sleep(3)
-      pesquisar = pyautogui.locateOnScreen('pesquisar.png', confidence = 0.8)
-      time.sleep(2)
-      pyautogui.moveTo(x= pesquisar.left+85, y= pesquisar.top+4)
-      time.sleep(1)
-      mxm.locator("#ext-gen877").click()
-      product = mxm.frame("1022_IFrame")
-      for index,row in planilha.iterrows():
+planilha = pd.read_excel("base_de_dados/Produtos para atualizar.xlsx",
+                         "Produtos", usecols=['Código', 'Produtos'], dtype='string')
+
+
+async def run(playwright):
+    chromium = playwright.chromium
+    browser = await chromium.launch(headless=False, timeout=10000)
+    context = await browser.new_context()
+    page = await context.new_page()
+    await page.goto("https://sistemas.mt.senac.br/")
+    async with context.expect_event("page") as event_info:
+        await page.locator(
+            "//img[@src='assets_sistemas\img\sistemas\mxm-treinamento.png']").click()
+    mxm = await event_info.value
+    await mxm.locator("#txfUsuario").fill("WLIK.SILVA")
+    await mxm.locator("#txfSenha").fill("Alterar@2022")
+    await mxm.locator("#ext-gen19").click()
+    time.sleep(2)
+    avaliacao = pyautogui.locateOnScreen('Avaliacao.png', confidence=0.9)
+    time.sleep(2)
+    if (avaliacao != None):
+        await mxm.locator(
+            "//*[@id='conpass-tag']/div/div/div[2]/div[1]/div[1]").click()
+    time.sleep(3)
+    await mxm.locator("#tgfBusca").fill("1022")
+    await mxm.locator("#ext-gen37").click()
+    frameSearch = mxm.frame_locator("//iframe[contains(@id,'_BUSCA__IFrame')]")
+    await frameSearch.locator("//a", has_text="Produto").click()
+    frameProduct = mxm.frame_locator("//iframe[contains(@id,'1022_IFrame')]")
+    for index, row in planilha.iterrows():
         codigo = row['Código']
         print(str(codigo))
         produtos = row['Produtos']
         print(str(produtos))
         print(index)
-        product.fill("#hpfCodigo", "")
-        product.fill("#hpfCodigo", str(codigo))
-        # time.sleep(1)
-        mxm.keyboard.press('Tab')
-        # time.sleep(1)
-        product.locator("#chkControleLiberadoParaMovimentacao").uncheck()
+        await frameProduct.locator("#hpfCodigo").fill('')
+        await frameProduct.locator("#hpfCodigo").fill(str(codigo))
+        await mxm.keyboard.press('Tab')
+        await frameProduct.locator("#chkControleLiberadoParaMovimentacao").uncheck()
         inativo = "inativo - "
-        product.locator("#txfDescricao").fill(inativo.upper()+str(produtos).upper())
-        # time.sleep(1)
+        await frameProduct.locator("#txfDescricao").fill(
+            inativo.upper()+str(produtos).upper())
         tipo_do_item = "07 - MATERIAL DE USO E CONSUMO"
-        product.locator("#hpcTipoItem").fill(tipo_do_item)
-        # time.sleep(0.5)
-        mxm.keyboard.press('Tab')
-        # time.sleep(1)
-        product.locator("#ext-gen26").click()
+        await frameProduct.locator("#hpcTipoItem").fill(tipo_do_item)
+        await mxm.keyboard.press('Tab')
+        await frameProduct.locator("#ext-gen26").click()
         time.sleep(3)
-      time.sleep(5)
-      tempoExec = time.time() -t1
-      print("Quantidade de alterações "+index+"\n tempo de execução ---->>> ", tempoExec)
-      browser.close()
+        tempos = time.time() - t1
+        if(index == 500):
+            print("Print "+index+" items alterados em --->> "+tempos+"")
+        if(index == 1000):
+            print("Print "+index+" items alterados em --->> "+tempos+"")
+        if(index == 2000):
+            print("Print "+index+" items alterados em --->> "+tempos+"")
+        if(index == 4000):
+            print("Print "+index+" items alterados em --->> "+tempos+"")
+    time.sleep(5)
+    tempoExec = time.time() - t1
+    print("Quantidade de alterações "+index +
+          "\n tempo de execução ---->>> ", tempoExec)
+    await browser.close()
+
+
+async def main():
+    async with async_playwright() as playwright:
+        await run(playwright)
+asyncio.run(main())
